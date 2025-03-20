d3b3becb387e09c4f2fb4e082e765fc8e845f4dd
mehak <mehak0878.be21@chitkara.edu.in>
2025-03-21 00:21:13 +0530

    Initial commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose")
 
 const connect=async()=>{
-    await mongoose.connect('mongodb+srv://luckygovindrao18:YvPikKGB4RYHkZMY@cluster0.z9kqdwq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+    await mongoose.connect('mongodb+srv://mehakwalia71:<db_password>@cluster0.2hf4gzl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 }
 
 module.exports=connect;
